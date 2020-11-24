2d06da24d3d9ea7bdf2b4a925f5cac8112cc3084
jemealBailey <58152960+jemealBailey@users.noreply.github.com>
2020-11-24 00:33:31 -0500

    change username_err to email err....fixing error

--- loginStaff.php
+++ loginStaff.php
@@ -1,78 +1,78 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
   	header("location: index.php");
   	exit;
 }
- 
+
 // Include config file
 include_once "config.php";
- 
+
 // Define variables and initialize with empty values
 $email = $password = $staffId = "";
 $email_err = $password_err = $staffId_err = "";
- 
+
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+
     // Check if email is empty
     if(empty(trim($_POST["email"]))){
         $email_err = "Please enter Email.";
     } else{
         $email = trim($_POST["email"]);
 	}
-	
+
 	// Check if staffId is empty
     if(empty(trim($_POST["staffId"]))){
         $staffId_err = "Please enter Staff ID.";
     } else{
         $staffId = trim($_POST["staffId"]);
     }
-    
+
     // Check if password is empty
     if(empty(trim($_POST["password"]))){
         $password_err = "Please enter your password.";
     } else{
         $password = trim($_POST["password"]);
     }
-    
+
     // Validate credentials
     if(empty($email_err) && empty($password_err)){
         // Prepare a select statement
         $sql = "SELECT StaffID, Name, Email, Password, Type FROM Staff WHERE Email = ? OR StaffID = ?";
-        
+
         if($stmt = mysqli_prepare($conn, $sql)){
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "ss", $param_email,$param_staffId);
-            
+
             // Set parameters
 			$param_email = $email;
 			$param_staffId = $staffId;
-            
+
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Store result
                 mysqli_stmt_store_result($stmt);
-                
+
                 // Check if email or staffId exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                if(mysqli_stmt_num_rows($stmt) == 1){
                     // Bind result variables
                     mysqli_stmt_bind_result($stmt, $staffId, $staffName, $email, $hashed_password, $type);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             // Password is correct, so start a new session
                             session_start();
-                            
+
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
 							$_SESSION["staffId"] = $staffId;
 							$_SESSION["staffName"] = $staffName;
 							$_SESSION["email"] = $email;
-							$_SESSION["type"] = $type;                              
-                            
+							$_SESSION["type"] = $type;
+
                             // Redirect user to welcome page
                             header("location: index.php");
                         } else{
@@ -93,7 +93,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
-    
+
     // Close connection
     mysqli_close($conn);
 }
@@ -104,7 +104,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 		<style>
 			* {box-sizing: border-box;}
-			body { 
+			body {
 				margin: 0;
 				font-family: Arial, Helvetica, sans-serif;
 			}
@@ -121,7 +121,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 				text-align: center;
 				padding: 12px;
 				text-decoration: none;
-				font-size: 12px; 
+				font-size: 12px;
 				line-height: 12px;
 				border-radius: 4px;
 			}
@@ -151,10 +151,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 					display: block;
 					text-align: left;
 				}
-					  
+
 				.header-right {
 					float: none;
-				}	  
+				}
 			}
 			.bg-img {
 			/* The image used */
@@ -166,7 +166,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 				position: relative;
 			}
 		</style>
-		
+
         <html>
        		<head>
           		<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -249,50 +249,49 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 							</div>
 					 </nav>
             	</header>
-	         
-         	</head>              
+
+         	</head>
      		<body>
-            	<div class="bg-img">     
-					<main>	<br/>	   
+            	<div class="bg-img">
+					<main>	<br/>
                     	<div id="page-wrapper">
 	                 		<section id="region-main" class="col-12">
 								<div align = "center">
-									<form method="POST" action=" "> 
+									<form method="POST" action=" ">
 										<div class="imgcontainer">
-											<img src="imgs/doctor.png" alt ="logo" style="width:100px">  
+											<img src="imgs/doctor.png" alt ="logo" style="width:100px">
 										</div>
 										<div>
 											<label for="email"><b>Email</b></label>
-											<input type="text" placeholder="Enter Email" title="No Space is allowed" 
+											<input type="text" placeholder="Enter Email" title="No Space is allowed"
 												name="email" value="<?php echo $email ?>"></br>
-												<?php echo $username_err; ?>
+												<?php echo $email_err; ?>
 										</div>
 
 										<div>
 											<label for="staffId"><b>Staff ID</b></label>
-											<input type="text" placeholder="Enter Staff ID" title="No Space is allowed" 
+											<input type="text" placeholder="Enter Staff ID" title="No Space is allowed"
 												name="staffId" value="<?php echo $email ?>"></br>
-												<?php echo $username_err; ?>
+												<?php echo $email_err; ?>
 										</div>
-										
+
 										<div>
 											<label for="psw"><b>Password</b></label>
 											<input type="password" placeholder="Enter Password" name="password" required></br>
 											<?php echo $password_err; ?>
 										</div>
-										
-							
+
+
 										<button type="submit" class="loginbtn">Login </button>
 										<label>
 											<input type="checkbox" checked="checked" name="remember"> Remember me</br>
-											<span class="psw">Forgot <a href="reset_password.php">password?</a></span>		 
+											<span class="psw">Forgot <a href="reset_password.php">password?</a></span>
 										</label></br></br></br>
 									</form>
 								</div>
-						    </section> 
+						    </section>
 			            </div>
            			</main>
             	</div>
-       		</body>  
+       		</body>
 		</html>
-
