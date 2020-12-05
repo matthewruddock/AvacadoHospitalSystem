2369242b9ee54d387119fbd831b54507f9abff69
1700237 <abigail_nembhard23@yahoo.com>
2020-12-05 11:15:09 -0500

    Css change to index

--- index.php
+++ index.php
@@ -38,33 +38,34 @@
 <!-- Links (sit on top) -->
 
 <div class="w3-top">
-
+    
   <div class=<?php echo '"'.$rowcol.'"';?>>
     <div class=<?php echo '"'.$navClass.'"';?>>
-      <a href="#" class=<?php echo '"'.$AClass.'"';?>>Home</a>
-    </div>
-    <div class=<?php echo '"'.$navClass.'"';?>>
-      <a href="#plans" class=<?php echo '"'.$AClass.'"';?>>Appointments</a>
-    </div>
-    <div class=<?php echo '"'.$navClass.'"';?>>
-      <a href="#about" class=<?php echo '"'.$AClass.'"';?>>About</a>
-    </div>
-    <div class=<?php echo '"'.$navClass.'"';?>>
-      <a href="BMI.php" class=<?php echo '"'.$AClass.'"';?>>BMI</a>
+	<ul>
+      <li><a href="#" class=<?php echo '"'.$AClass.'"';?>>Home</a></li>
+   
+   
+      <li><a href="#plans" class=<?php echo '"'.$AClass.'"';?>>Appointments</a></li>
+    
+ 
+      <li><a href="#about" class=<?php echo '"'.$AClass.'"';?>>About</a></li>
+    
+
+      <li><a href="BMI.php" class=<?php echo '"'.$AClass.'"';?>>BMI</a></li>
     </div>
     <?php
      if(($_SESSION["role"]=="Doctor")||($_SESSION["role"]=="Nurse")){
     echo "  <div class= '".$navClass."'>
-        <a href='editProfile.php' class='".$AClass."'>Edit Profile</a>
+        <li><a href='editProfile.php' class='".$AClass."'>Edit Profile</a></li>
       </div>
       <div class='".$navClass."'>
-         <a href='LogPatient.php' class='".$AClass."'>Login Patient</a>
+         <li><a href='LogPatient.php' class='".$AClass."'>Login Patient</a></li>
        </div>
        <div class='".$navClass."'>
-          <a href='registerPatient.php' class='".$AClass."'>Register Patient</a>
+          <li><a href='registerPatient.php' class='".$AClass."'>Register Patient</a></li>
         </div>
         <div class='".$navClass."'>
-           <a href='searchPatient.php' class='$AClass'>Search Patient</a>
+           <li><a href='searchPatient.php' class='$AClass'>Search Patient</a></li>
          </div>";
 
 
@@ -92,7 +93,7 @@
       <a href="#contact" class="w3-button w3-block">Contact</a>
 
     </div>
-
+</ul>
   </div>
 </div>
 
@@ -207,7 +208,7 @@
     </div>
 
     <div class="w3-third w3-margin-bottom">
-      <ul class="w3-ul w3-border w3-center w3-hover-shadow">
+      <ul class="w3-ul w3-border w3-center w3-hover-shadow w3-white">
         <li class="w3-black w3-xlarge w3-padding-32">MRI/X-RAY</li>
         <li class="w3-padding-16"><b>X-ray radiography</b>Detects bone fractures, certain tumors and other abnormal masses, pneumonia, some types of injuries, calcifications, foreign objects, dental problems. </li>
         <li class="w3-padding-16"><b>Mammography</b> A radiograph of the breast that is used for cancer detection and diagnosis.</li>
@@ -236,10 +237,10 @@
       </div>
     </div>
 
-    <div class="w3-third w3-margin-bottom">
+    <div class="w3-third w3-margin-bottom w3-white">
       <div class="w3-card-4">
         <img src="imgs/logo.png" alt="Mike" style="width:40%">
-        <div class="w3-container">
+        <div class="w3-container w3-white">
           <h4>Avacodo Medical Center </h4>
           <h3><p class="w3-opacity">Was Founded IN 1997.</p></h3>
           <p></bold>Mission:</bold>
@@ -252,10 +253,10 @@
       </div>
     </div>
 
-    <div class="w3-third w3-margin-bottom">
+    <div class="w3-third w3-margin-bottom w3-white">
       <div class="w3-card-4">
         <img src="imgs/news.png" alt="Jane" style="width:33%">
-        <div class="w3-container">
+        <div class="w3-container w3-white">
           <h3>News And Post</h3>
           <p class="w3-opacity"></p>
           <p>Due To Covid-19 Mask must be worn at all times at our locations. </p>
@@ -268,7 +269,7 @@
 
   <!-- Contact -->
   <div class="w3-center w3-padding-64" id="contact">
-    <br/> <br/><span class="w3-xlarge w3-bottombar w3-border-dark-grey w3-padding-16">Contact Us</span>
+    <br/> <br/><span class="w3-xlarge w3-bottombar w3-border-dark-grey w3-padding-16"><strong>Contact Us</strong></span>
   </div>
 
   <form class="w3-container" action="/action_page.php" target="_blank">
