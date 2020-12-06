d02c30cb5cfdb752a5b9ef098f28d83cdc9c61da
1700237 <abigail_nembhard23@yahoo.com>
2020-12-05 21:01:03 -0500

    Update on CSS

--- signupStaff.php
+++ signupStaff.php
@@ -142,7 +142,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- Add icon library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="amc_style.css">
+
     <html>
         <header>
 			<nav>
