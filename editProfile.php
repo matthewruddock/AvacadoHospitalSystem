6f68708b3b8b490414aa12da15654dc201b4c02a
jemealBailey <58152960+jemealBailey@users.noreply.github.com>
2020-12-06 23:13:06 -0500

    update

--- editProfile.php
+++ editProfile.php
@@ -9,9 +9,105 @@ foreach($_SESSION as $key => $value)	//store SESSION values in to local variable
 }
 ?>
 <!DOCTYPE html>
-<html>
-        <head>
-            <meta charset="utf-8">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+			<!-- Add icon library -->
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+			<style>
+					* {box-sizing: border-box;}
+
+					body {
+					  margin: 0;
+					  font-family: Arial, Helvetica, sans-serif;
+					}
+
+					.header {
+					  overflow: hidden;
+					  background-color: #f1f1f1;
+					  padding: 0px 10px;
+					}
+
+					.header a {
+					  float: left;
+					  color: black;
+					  text-align: center;
+					  padding: 12px;
+					  text-decoration: none;
+					  font-size: 12px;
+					  line-height: 12px;
+					  border-radius: 4px;
+					}
+
+					.header a.logo {
+					  font-size: 20px;
+					  font-weight: bold;
+					}
+
+					.header a:hover {
+					  background-color: #ddd;
+					  color: black;
+					}
+
+					.header a.active {
+					  background-color: dodgerblue;
+					  color: white;
+					}
+
+					.header-right {
+					  float: right;
+					}
+
+					@media screen and (max-width: 500px) {
+					  .header a {
+						float: none;
+						display: block;
+						text-align: left;
+					  }
+
+					  .header-right {
+						float: none;
+					  }
+
+					}
+					   </style>
+						<style>
+					   .bg-img {
+					  /* The image used */
+					  background-image: url("imgs/form_background.jpg");
+
+					  min-height: 580px;
+
+					  /* Center and scale the image nicely */
+					  background-position: ;
+					  background-repeat: no-repeat;
+					  background-size: cover;
+					  position: relative;
+					}
+             </style>
+          <html>
+       <head>
+
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+
+                   <header>
+			    <nav>
+							<div class="header"></br>
+								  <img src="imgs/logo.png" style="width:10%">
+								  </div>
+
+								  <div class="header">
+
+								  </div>
+								  <div class="header">
+
+
+								 <div class="header-right">
+								<a href="newindex.php"> <i class="glyphicon glyphicon-home"></i>HOME</a>
+
+
+                                </div>
+				 </nav>
+				 <div class="bg-img">
+				 </br>
             <title>Edit Profile</title>
             <link rel="stylesheet" href="css/style.css" />
             <link rel="stylesheet" href="bootstrap.css"/>
@@ -24,7 +120,7 @@ foreach($_SESSION as $key => $value)	//store SESSION values in to local variable
 
             <?php
             if($_SESSION['type']!="Guest"){
-                
+
                 $ID=$_SESSION['staffId'];
                 $SelQuery= "SELECT * FROM staff  WHERE StaffID='$ID'";
                 $result=mysqli_query($conn,$SelQuery);
